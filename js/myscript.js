196010a6c4f5683b5286acf50ab66f5d7bfcc22a
Ahmed Hafez <61163635+AhmedHafez7-Eng@users.noreply.github.com>
2021-08-01 01:43:55 +0200

    Update myscript.js file

--- js/myscript.js
+++ js/myscript.js
@@ -1,103 +1,109 @@
-/* -----------------
+$(document).ready(function () {
+	$(this).scrollTop(0);
+	/* -----------------
 		Loader && AOS Animation
 	--------------*/
-window.addEventListener("load", function () {
-	/* -------------------- Page Loader--------------------- */
-	document.querySelector(".pageLoader").classList.add("fade-out");
-	setTimeout(function () {
-		document.querySelector(".pageLoader").style.display = "none";
-	}, 600);
-	/* -------------------- Animation On Scroll--------------------- */
-	AOS.init();
-});
+	window.addEventListener("load", function () {
+		/* -------------------- Page Loader--------------------- */
+		document.querySelector(".pageLoader").classList.add("fade-out");
+		setTimeout(function () {
+			document.querySelector(".pageLoader").style.display = "none";
+		}, 600);
+		// Animation On Scrolling
+		wow = new WOW({
+			boxClass: 'wow',
+			animateClass: 'animated',
+			offset: 0,
+			live: true,
+			// triggers animations on mobile devices
+			mobile: true
+		});
+		wow.init();
+	});
 
-
-
-/* -----------------
+	/* -----------------
 		Making Navbar Sticky
 	--------------*/
 
-window.addEventListener('scroll', function () {
+	window.addEventListener('scroll', function () {
 
-	const header = document.querySelector('header');
-	header.classList.toggle("sticky", window.scrollY > 0);
-});
-/* -----------------
-		Links Add Active Class on click 	(Navbar Section)
-	--------------*/
+		const header = document.querySelector('header');
+		header.classList.toggle("sticky", window.scrollY > 0);
+	});
+	/* -----------------
+			Links Add Active Class on click 	(Navbar Section)
+		--------------*/
 
-$('header ul li a').click(function () {
+	$('header ul li a').click(function () {
 
-	$(this).parent().addClass('activeColor').siblings().removeClass('activeColor');
-});
+		$(this).parent().addClass('activeColor').siblings().removeClass('activeColor');
+	});
 
-/* -----------------
-		Trigger Toggle Navbar Menu
-	--------------*/
+	/* -----------------
+			Trigger Toggle Navbar Menu
+		--------------*/
 
-function togglemenu() {
-	var menuToggle = document.querySelector('.menuToggle');
-	var menu = document.querySelector('.navlinks');
-	menuToggle.classList.toggle('activenav');
-	menu.classList.toggle('activenav');
-}
-
-/* -----------------
-		Trigger To Top Button
-	--------------*/
-
-$(window).scroll(function () {
-	if ($(this).scrollTop()) {
-		$('#toTop').fadeIn();
-	} else {
-		$('#toTop').fadeOut();
+	function togglemenu() {
+		var menuToggle = document.querySelector('.menuToggle');
+		var menu = document.querySelector('.navlinks');
+		menuToggle.classList.toggle('activenav');
+		menu.classList.toggle('activenav');
 	}
-});
 
-$("#toTop").click(function () {
-	//1 second of animation time
-	//html works for FFX but not Chrome
-	//body works for Chrome but not FFX
-	//This strange selector seems to work universally
-	$("html, body").animate({
-		scrollTop: 0
-	}, 40);
-});
+	/* -----------------
+			Trigger To Top Button
+		--------------*/
 
-/* -----------------
-		Show Only 6 boxes and hide more
-	--------------*/
+	$(window).scroll(function () {
+		if ($(this).scrollTop()) {
+			$('#toTop').fadeIn();
+		} else {
+			$('#toTop').fadeOut();
+		}
+	});
 
-let divs = document.querySelectorAll(".menu .content .box");
-let divsArray = Array.from(divs);
-for (const i in divsArray) {
-	if (i > 5) {
-		divsArray[i].classList.add("hidden");
-	} else {
-		continue;
+	$("#toTop").click(function () {
+		//1 second of animation time
+		//html works for FFX but not Chrome
+		//body works for Chrome but not FFX
+		//This strange selector seems to work universally
+		$("html, body").animate({
+			scrollTop: 0
+		}, 40);
+	});
+
+	/* -----------------
+			Show Only 6 boxes and hide more
+		--------------*/
+
+	let divs = document.querySelectorAll(".menu .content .box");
+	let divsArray = Array.from(divs);
+	for (const i in divsArray) {
+		if (i > 5) {
+			divsArray[i].classList.add("hidden");
+		} else {
+			continue;
+		}
 	}
-}
+	/* -----------------
+			Filter Content && Item Popup
+		--------------*/
 
-/* -----------------
-		Filter Content && Item Popup
-	--------------*/
 
-$(document).ready(function () {
-
-	$('.buttons').click(function () {
+	$('.menu .controls .buttons').click(function () {
 
 		$(this).addClass('active').siblings().removeClass('active');
 
 		var filter = $(this).attr('data-filter');
 
 		if (filter == 'all') {
-			$('.box').show(400);
+			$('.menu .content .box').show(400);
 			//----------- Show Only 6 boxes and hide more
 			$('.box.hidden').hide(1);
 
 		} else {
-			$('.box').not('.' + filter).hide(200);
-			$('.box').filter('.' + filter).show(400);
+			$('.menu .content .box').not('.' + filter).hide(200);
+			$('.menu .content .box').filter('.' + filter).show(400);
 		}
 	});
 
@@ -127,14 +133,3 @@ $(document).ready(function () {
 		}
 	}
 });
-
-
-/* -----------------
-			Add an Attribute to all images in page
-		--------------*/
-// var img = document.body.getElementsByTagName("img");
-// var i = 0;
-// while (i < img.length) {
-// 	img[i].setAttribute("loading", "lazy");
-// 	i++;
-// }
